97f8dcd021742000bbf537109c881650179aac20
Matt Nawrocki <mjnrock@umich.edu>
2018-06-01 13:46:00 -0400

    redesign

---
+++ src/_old/second/components/App.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as Messages from "../actions/Messages";
+import StaxPax from "./staxpax";
+
+class App extends Component {
+	render() {
+		return (
+			<main stax="container">
+				<StaxPax.Components.Card.Card>					
+	<StaxPax.Components.Card.Segment.Synopsis>			
+		<StaxPax.Components.Card.Element.Pane.TitleBanner
+			title="Shooty Hoops"
+			desc="Shootin' some scrud, ya' heard? #Perdverts"
+		/>
+
+		<StaxPax.Components.Card.Element.Pane.Tags
+			tags="men 21 2v2 3v3 beer-league"
+		/>	
+		
+		<StaxPax.Components.Card.Element.Pane.Pane cols="3">
+			<StaxPax.Components.Card.Element.Emblem.Metric
+				icon="directions_car"
+				unit="mi"
+				value="8.5"
+			/>
+			<StaxPax.Components.Card.Element.Emblem.SportIcon 
+				sport="bball"
+			/>
+			<StaxPax.Components.Card.Element.Emblem.Metric 
+				icon="schedule"
+				unit="pm"
+				value="5:30"
+			/>
+		</StaxPax.Components.Card.Element.Pane.Pane>
+		
+		<StaxPax.Components.Card.Element.Pane.Map
+			url="/assets/images/map.png"
+			sport="bball"
+		/>
+
+		<StaxPax.Components.Card.Element.Pane.Pane cols="1">
+			<StaxPax.Components.Card.Element.Pane.TitleBanner
+				size="s"
+				title="Basketball America"
+				desc="257 Clarkston Rd., Lake Orion, MI 48362"
+			/>
+			<StaxPax.Components.Card.Element.Emblem.Rating
+				value="2.1"
+				label="Rating"
+			/>
+		</StaxPax.Components.Card.Element.Pane.Pane>
+
+	</StaxPax.Components.Card.Segment.Synopsis>
+</StaxPax.Components.Card.Card>
+			</main>
+		);
+	}
+}
+
+export default connect(
+	(state) => {
+		return {
+			messages: state.messages
+		};
+	},
+	(dispatch) => {
+		return {
+			GetMessages: (arg) => dispatch(Messages.GetMessages(arg))
+		};
+	}
+)(App);
