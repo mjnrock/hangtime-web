f5f9ecc251ad1cf154a18e556e0d07e8d79c7a91
mjnrock <mjnrock@umich.edu>
2018-07-08 22:02:59 -0400

    ws

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ import App from "./App";
 
 const store = initStore();
 
+let ws = new WebSocket("ws://localhost:1337/ws");
+
+ws.onopen = function(conn) {
+	console.log(conn);
+	ws.send("Hey!");
+};
+
+ws.onmessage = function (conn) { 
+	console.log(conn);
+};
+
+ws.onclose = function(conn) { 
+	console.log(conn);
+};
+
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
